46857f7903dd6439407680b341219da8039ba775
Tista Manandhar <tistamanandhar@Tistas-MacBook-Pro.local>
2023-11-14 01:37:52 -0600

    P10 extreme bonus

---
+++ P10/extreme_bonus/Time.h
@@ -0,0 +1,38 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+
+class Time {
+private:
+    int _hour;
+    int _minute;
+    int _second;
+
+public:
+    Time(int hour, int minute, int second);
+    Time();
+
+    void rationalize();
+
+    int operator[](int index) const;
+
+    Time operator+(const Time& other) const;
+    Time& operator++();
+    Time operator++(int);
+    bool operator==(const Time& other) const;
+    bool operator!=(const Time& other) const;
+    bool operator<(const Time& other) const;
+    bool operator>(const Time& other) const;
+    bool operator<=(const Time& other) const;
+    bool operator>=(const Time& other) const;
+
+    Time operator+(int seconds) const;
+    friend Time operator+(int seconds, const Time& time);
+
+    friend std::ostream& operator<<(std::ostream& os, const Time& time);
+    friend std::istream& operator>>(std::istream& is, Time& time);
+};
+
+#endif // TIME_H
+
